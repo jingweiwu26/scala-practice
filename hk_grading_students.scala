9322415d91deca0ada70e4a5169f561aa87fd3b8
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2018-12-10 21:32:59 -0500

    Update hk_grading_students.scala

--- hk_grading_students.scala
+++ hk_grading_students.scala
@@ -1,6 +1,4 @@
 def gradingStudents(grades: Array[Int]): Array[Int] = {
-        def gradingStudents(grades: Array[Int]): Array[Int] = {
-        val helper = (x: Int) => if ((x % 5 == 3 || x % 5 == 4) && (x > 37)) x / 5 * 5 + 5 else x
-        grades.map(helper)
-    }
+    val helper = (x: Int) => if ((x % 5 == 3 || x % 5 == 4) && (x > 37)) x / 5 * 5 + 5 else x
+    grades.map(helper)
     }
